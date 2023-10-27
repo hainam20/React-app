6a33e83fb8d67f556d1fbd6d6fee2504914052b1
20520642 <20520642@gm.uit.edu.vn>
2023-10-27 17:00:12 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from "./pages/home/Home";
+import "./index.css";
 import Control from "./components/control/Control";
 
-import "./index.css";
-
 function App() {
-    return (
-        <div>
-            <Topbar />
-            <div className="flex">
-                <Sidebar />
-                <div className="flex w-full">
-                    <Home />
-                    <Control />
-                </div>
-            </div>
-        </div>
-    );
-};
+  return (
+    <div>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Home />
+        <Control />
+      </div>
+    </div>
+  );
+}
 
 export default App;
--- src/components/featuredInfo/FeturedInfo.jsx
+++ src/components/featuredInfo/FeturedInfo.jsx
@@ -1,37 +1,31 @@
-import io from 'socket.io-client';
-import React , {useState, useEffect} from 'react';
-
-import "./featuredInfo.css";
+import "./featuredInfo.css"
+import io from 'socket.io-client'
+import React , {useState, useEffect} from 'react'
 
 const socket = io("localhost:5000/", {
     transports: ["websocket"],
     cors: {
       origin: "http://localhost:3000/",
     },
-});
-
-export default function FeturedInfo() {
-
-    const [data, setData] = useState([]);
-
+  });
+    export default function FeturedInfo() {
+    const [data, setData] = useState([])
     useEffect(() => {
         socket.on("connect", () => {
             console.log('Connected to Flask server');
         });
-
         socket.on('mqtt_data', (data) => {
             console.log('Received new data:', data);
             setData(data);
-        });
-
+          });
         return () => {
-            socket.off();
+        socket.off();
         };
-    }, []);
-
+        }, []);
   return (
     <div className="featured mb-4">
         <div className="featuredItem bg-white hover:scale-105 hover:transition-all">
+            <h1> Node {1}</h1>
             <span className="featuredTitle text-blue-400">Temperature</span>
             <div className="featuredTemperatureContainer">
                 <span className="featuredTemperature">{data.temp || '_'} °C</span>
@@ -47,9 +41,8 @@ export default function FeturedInfo() {
             <span className="featuredTitle text-blue-400">Soil Measure</span>
             <div className="featuredTemperatureContainer">
                 <span className="featuredTemperature">{data.adc_val || '_'}%</span>
-        
             </div>
         </div>
     </div>
-  );
-};
+  )
+}
